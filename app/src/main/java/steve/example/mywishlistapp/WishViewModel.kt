6d0cfef20552901a30b531c4523434aff709fe3a
StevenTD <steven.delarosa566@gmail.com>
2024-10-02 10:48:34 +0800

    prepping viewModel

--- app/src/main/java/steve/example/mywishlistapp/WishViewModel.kt
+++ app/src/main/java/steve/example/mywishlistapp/WishViewModel.kt
@@ -4,9 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import steve.example.mywishlistapp.data.Wish
+import steve.example.mywishlistapp.data.WishRepository
 
 // Store data, load, edit, everything.
-class WishViewModel:ViewModel() {
+class WishViewModel(
+    private val wishRepository: WishRepository
+):ViewModel() {
 
     var wishTitleState by mutableStateOf("")
     var wishDescriptionState by mutableStateOf("")
@@ -18,5 +26,25 @@ class WishViewModel:ViewModel() {
 
     fun onWishDescriptionChanged(newString:String){
         wishDescriptionState = newString
+    }
+
+    lateinit var getAllWishes: Flow<List<Wish>>
+
+    init {
+        viewModelScope.launch {
+            getAllWishes = wishRepository.getWishes()
+        }
+    }
+
+    fun addWish(wish: Wish){
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.addAWish(wish=wish)
+        }
+    }
+
+    fun updateWish(wish: Wish){
+        viewModelScope.launch(Dispatchers.IO) {
+            wishRepository.updateAWish(wish=wish)
+        }
     }
 }
