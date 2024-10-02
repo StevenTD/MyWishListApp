d67a7738dc98b507df8100ee57c1c2ad8cb84da0
StevenTD <steven.delarosa566@gmail.com>
2024-10-02 11:20:01 +0800

    added Graph to use/build database file

---
+++ app/src/main/java/steve/example/mywishlistapp/Graph.kt
@@ -0,0 +1,21 @@
+package steve.example.mywishlistapp
+
+import android.content.Context
+import androidx.room.Room
+import steve.example.mywishlistapp.data.WishDatabase
+import steve.example.mywishlistapp.data.WishRepository
+
+object Graph {
+    lateinit var database: WishDatabase
+
+    val wishRepository by lazy{ // By lazy, initialize once only needed and only once
+        WishRepository(
+            wishDao = database.wishDao()
+        )
+
+    }
+
+    fun provide(context: Context){
+        database = Room.databaseBuilder(context, WishDatabase::class.java, "wishlist.db").build()
+    }
+}
